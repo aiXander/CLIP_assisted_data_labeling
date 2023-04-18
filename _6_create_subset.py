651969b792ebc24145516d310acc2b64e3980720
xander <xandersteenbruggex@gmail.com>
2023-04-18 09:45:13 -0700

    fix test mode

--- _6_create_subset.py
+++ _6_create_subset.py
@@ -18,13 +18,14 @@ def copy_data(args, output_suffix = '_subset'):
     database = database.loc[database["predicted_label"] < args.max_score]
     print(f"Found {len(database)} rows with {args.min_score} < predicted_label < {args.max_score}")
 
-    if args.test:
-        return
-
     output_suffix = f'_{args.min_score:.2f}_to_{args.max_score:.2f}' + output_suffix
     output_folder = os.path.join(args.input_dir + output_suffix)
-    os.makedirs(output_folder, exist_ok=True)
-    
+
+    if args.test:
+        print(f"Running script in TEST MODE: Not actually copying any files to {output_folder}")
+    else:
+        os.makedirs(output_folder, exist_ok=True)
+
     # Loop over the uuids in the database and copy the corresponding files to the output folder:
     print(f"Copying files to {output_folder}...")
     counter = [0] * len(args.extensions)
@@ -43,17 +44,19 @@ def copy_data(args, output_suffix = '_subset'):
             filename = uuid + ext
             input_path = os.path.join(args.input_dir, filename)
             output_path = os.path.join(output_folder, filename)
-            shutil.copy(input_path, output_path)
+            if not args.test:
+                shutil.copy(input_path, output_path)
             counter[args.extensions.index(ext)] += 1
 
     for ext, count in zip(args.extensions, counter):
         print(f"Copied {count} files with extension {ext} to {output_folder}")
         
-    # count the total number of img files in the output folder:
-    img_extensions = ('.jpg', '.jpeg', '.png')
-    n_img_files = len([f for f in os.listdir(output_folder) if f.endswith(img_extensions)])
-    # append the total number of imgs to the output foldername:
-    os.rename(output_folder, output_folder + f"_{n_img_files}_imgs")
+    if not args.test:
+        # count the total number of img files in the output folder:
+        img_extensions = ('.jpg', '.jpeg', '.png')
+        n_img_files = len([f for f in os.listdir(output_folder) if f.endswith(img_extensions)])
+        # append the total number of imgs to the output foldername:
+        os.rename(output_folder, output_folder + f"_{n_img_files}_imgs")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -61,9 +64,9 @@ if __name__ == "__main__":
     parser.add_argument('--min_score', type=float, help='Input directory')
     parser.add_argument('--max_score', type=float, default=1.0, help='Maximum score to consider')
     parser.add_argument('--extensions', nargs='+', default=['.jpg', '.txt', '.pt'], help='Extensions to consider')
-    parser.add_argument('--test', action='store_true', help='Test mode')
     parser.add_argument('--min_aspect_ratio', type=float, default=0.25, help='Minimum aspect ratio of imgs to consider')
     parser.add_argument('--max_aspect_ratio', type=float, default=4.00, help='Maximum aspect ratio of imgs to consider')
+    parser.add_argument('--test', action='store_true', help='Test mode, wont actually copy anything')
     args = parser.parse_args()
 
     copy_data(args)

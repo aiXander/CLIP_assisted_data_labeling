1104f272c14ed78dbe921c0ac1e4e15798b839ad
xander <xandersteenbruggex@gmail.com>
2023-08-01 04:07:28 -0700

    update

--- predict_simple.py
+++ predict_simple.py
@@ -2,10 +2,9 @@ import os
 import numpy as np
 import pickle
 import torch
-from utils.nn_model import device, SimpleFC
-import torch
 from PIL import Image
 
+from utils.nn_model import SimpleFC
 from _1_embed_with_CLIP import CustomImageDataset, CLIP_Model
 
 def load_model(model_path, device):
@@ -40,26 +39,30 @@ def predict_score(pil_img, aesthetic_regressor, active_clip_models):
         all_img_features.append(features)
 
     features = torch.stack(all_img_features)
-    score = aesthetic_regressor(features.to(device).float()).cpu().numpy().squeeze()
+    score = aesthetic_regressor(features.to(clip_model.device).float()).cpu().numpy().squeeze()
     return score
 
 
 if __name__ == "__main__":
 
-    img_dir = "/home/xander/Projects/cog/xander_eden_stuff/xander/assets/garden"
-    list_of_img_paths = [os.path.join(img_dir, img_name) for img_name in os.listdir(img_dir) if '.jpg' in img_name]
-    model_path = "/home/xander/Projects/cog/xander_eden_stuff/xander/CLIP_assisted_data_labeling/models/combo_2023-07-31_06:20:57_8.1k_imgs_80_epochs_-1.0000_mse.pkl"
-    device     = "cpu"
-    
+    input_img_dir = "/home/xander/Projects/cog/xander_eden_stuff/xander/assets/garden"
+    model_path    = "/home/xander/Projects/cog/xander_eden_stuff/xander/CLIP_assisted_data_labeling/models/combo_2023-07-31_06:20:57_8.1k_imgs_80_epochs_-1.0000_mse.pkl"
+    device        = "cpu"
+
     # Load the scoring model:
     aesthetic_regressor = load_model(model_path, device)
 
-    # Load the CLIP models corresponding to this pretrained regressor:
+    # Load the CLIP models corresponding to this pretrained Aesthetic Regressor:
     active_clip_models = []
-    for model_name in aesthetic_regressor.clip_models:
-        active_clip_models.append(CLIP_Model(model_name, device = device))
+    for clip_model_name in aesthetic_regressor.clip_models:
+        active_clip_models.append(CLIP_Model(clip_model_name, device = device))
 
+    # Get all the img_paths:
+    list_of_img_paths = [os.path.join(input_img_dir, img_name) for img_name in os.listdir(input_img_dir) if '.jpg' in img_name]
+
+    print("\nPredicting aesthetic scores...")
     for image_path in list_of_img_paths:
-        score = predict_score(Image.open(image_path), aesthetic_regressor, active_clip_models)
+        pil_img = Image.open(image_path)
+        score = predict_score(pil_img, aesthetic_regressor, active_clip_models)
         print(f"Score: {score:.3f} for {os.path.basename(image_path)}")
 

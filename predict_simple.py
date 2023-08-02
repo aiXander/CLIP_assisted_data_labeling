89ebc1e19215aeab446f77d5162f1610982d99fc
Xander Steenbrugge <xandersteenbruggex@gmail.com>
2023-08-02 21:36:27 +0200

    Update predict_simple.py

--- predict_simple.py
+++ predict_simple.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
 
     print("\nPredicting aesthetic scores...")
     for image_path in list_of_img_paths:
-        score = aesthetic_regressor.predict_score(Image.open(image_path))
+        score, embedding = aesthetic_regressor.predict_score(Image.open(image_path))
         print(f"Score: {score:.3f} for {os.path.basename(image_path)}")
 

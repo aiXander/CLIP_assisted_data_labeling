bce805c898236112e5692db15c201885adb93cd3
xander <xandersteenbruggex@gmail.com>
2023-08-02 04:22:59 -0700

    refactor predict_simple

--- predict_simple.py
+++ predict_simple.py
@@ -1,46 +1,42 @@
 import os
-import numpy as np
-import pickle
 import torch
 from PIL import Image
 
-from utils.nn_model import SimpleFC
 from _1_embed_with_CLIP import CustomImageDataset, CLIP_Model
 
-def load_model(model_path, device):
-    model = torch.load(model_path, map_location = device)
-    model.to(device)
-    model.eval()
+class AestheticRegressor:
+    """
+    Aesthetic Regressor to predict the aesthetic score of images.
+    """
+    def __init__(self, model_path, device="cpu"):
+        self.device = device
+        self.load_model(model_path)
+        
+        # Load associated CLIP models
+        self.clip_models = [CLIP_Model(name, device=self.device) for name in self.model.clip_models]
+    
+    @torch.no_grad()
+    def load_model(self, model_path, verbose=1):
+        self.model = torch.load(model_path, map_location = self.device).to(self.device).eval()
+        if verbose:
+            print("Loaded regression model")
+            print(f"Aesthetic Regressor was trained on embeddings from CLIP models:")
+            print(self.model.clip_models)
+            print(f"Aesthetic Regressor used crops:")
+            print(self.model.crop_names)
 
-    # disable gradients:
-    for param in model.parameters():
-        param.requires_grad = False
-
-    print("Loaded regression model")
-    print(f"Aesthetic Regressor was trained on embeddings from CLIP models:")
-    print(model.clip_models)
-    print(f"Aesthetic Regressor used crops:")
-    print(model.crop_names)
-
-    return model
-
-def predict_score(pil_img, aesthetic_regressor, active_clip_models):
-    all_img_features = []
-    for clip_model in active_clip_models:
-        # This can be improved, but is fine for now:
-        img_dataset = CustomImageDataset([image_path], clip_model.img_resolution, aesthetic_regressor.crop_names)
-        crops, crop_names = img_dataset.extract_crops(pil_img)
-        features = clip_model.pt_imgs_to_features(crops)
-
-        # Reshape the features back into [batch_size x n_crops x dim]:
-        batch_size = 1
-        features = features.view(batch_size, -1, features.shape[-1])
-
-        all_img_features.append(features)
-
-    features = torch.stack(all_img_features)
-    score = aesthetic_regressor(features.to(clip_model.device).float()).cpu().numpy().squeeze()
-    return score
+    @torch.no_grad()
+    def predict_score(self, pil_img):
+        all_img_features = []
+        
+        for clip_model in self.clip_models:
+            img_dataset = CustomImageDataset([pil_img], clip_model.img_resolution, self.model.crop_names)
+            crops, _ = img_dataset.extract_crops(pil_img)
+            features = clip_model.pt_imgs_to_features(crops).unsqueeze(0)  # Add batch dimension
+            all_img_features.append(features)
+        
+        features = torch.stack(all_img_features)
+        return self.model(features.to(self.device).float()).item()  # Convert tensor to single float value
 
 
 if __name__ == "__main__":
@@ -49,20 +45,14 @@ if __name__ == "__main__":
     model_path    = "/home/xander/Projects/cog/xander_eden_stuff/xander/CLIP_assisted_data_labeling/models/combo_2023-08-02_03:48:00_8.1k_imgs_80_epochs_-1.0000_mse.pth"
     device        = "cpu"
 
-    # Load the scoring model:
-    aesthetic_regressor = load_model(model_path, device)
-
-    # Load the CLIP models corresponding to this pretrained Aesthetic Regressor:
-    active_clip_models = []
-    for clip_model_name in aesthetic_regressor.clip_models:
-        active_clip_models.append(CLIP_Model(clip_model_name, device = device))
+    # Load the scoring model (only do this once in a python session):
+    aesthetic_regressor = AestheticRegressor(model_path, device = device)
 
     # Get all the img_paths:
     list_of_img_paths = [os.path.join(input_img_dir, img_name) for img_name in os.listdir(input_img_dir) if '.jpg' in img_name]
 
     print("\nPredicting aesthetic scores...")
     for image_path in list_of_img_paths:
-        pil_img = Image.open(image_path)
-        score = predict_score(pil_img, aesthetic_regressor, active_clip_models)
+        score = aesthetic_regressor.predict_score(Image.open(image_path))
         print(f"Score: {score:.3f} for {os.path.basename(image_path)}")
 
